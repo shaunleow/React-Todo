6368d01f6a67cd62edb630acccc4873206c3febd
shaunleow <shaunleow@gmail.com>
2017-09-16 11:29:46 +0800

    Add timestamps

--- app/components/Todo.jsx
+++ app/components/Todo.jsx
@@ -1,8 +1,20 @@
-var React = require("react");
+var React = require('react');
+var moment = require('moment');
 
 var Todo = React.createClass({
   render: function() {
-    var { text, id, completed } = this.props;
+    var { text, id, completed, createdAt, completedAt } = this.props;
+    var renderDate = () => {
+      var message = 'Created ';
+      var timestamp = createdAt;
+
+      if (completed) {
+        message = 'Completed ';
+        timestamp = completedAt;
+      }
+
+      return message + moment.unix(timestamp).format('MMM Do YYY @ h:mm a');
+    };
 
     return (
       // using an anonymous arrow function instead of passing it down from React.createClass
@@ -11,7 +23,8 @@ var Todo = React.createClass({
           this.props.onToggle(id);
         }}>
         <input type="checkbox" checked={completed} />
-        {text}
+        <p>{text}</p>
+        <p>{renderDate()}</p>
       </div>
     );
   },
--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -1,18 +1,18 @@
-var React = require("react");
-var ReactDOM = require("react-dom");
-var expect = require("expect");
-var $ = require("jQuery");
-var TestUtils = require("react-addons-test-utils");
+var React = require('react');
+var ReactDOM = require('react-dom');
+var expect = require('expect');
+var $ = require('jQuery');
+var TestUtils = require('react-addons-test-utils');
 
-var TodoApp = require("TodoApp");
+var TodoApp = require('TodoApp');
 
-describe("TodoApp", () => {
-  it("should exist", () => {
+describe('TodoApp', () => {
+  it('should exist', () => {
     expect(TodoApp).toExist();
   });
 
-  it("should add todo to the todos state on handleAddTodo", () => {
-    var todoText = "test text";
+  it('should add todo to the todos state on handleAddTodo', () => {
+    var todoText = 'test text';
     var todoApp = TestUtils.renderIntoDocument(<TodoApp />);
 
     todoApp.setState({ todos: [] }); // set it into an empty array
@@ -20,13 +20,17 @@ describe("TodoApp", () => {
 
     // grab the text value of the todo that should've been added and verify it matches
     expect(todoApp.state.todos[0].text).toBe(todoText);
+    // additional test for createdAt
+    expect(todoApp.state.todos[0].createdAt).toBeA('number');
   });
 
-  it("should toggle completed value when handleToggle called", () => {
+  it('should toggle completed value when handleToggle called', () => {
     var todoData = {
       id: 11,
-      text: "Test features",
+      text: 'Test features',
       completed: false,
+      createdAt: 0,
+      completedAt: undefined,
     };
     var todoApp = TestUtils.renderIntoDocument(<TodoApp />);
     todoApp.setState({ todos: [todoData] });
@@ -37,5 +41,24 @@ describe("TodoApp", () => {
     todoApp.handleToggle(11);
     // Verify that value changed
     expect(todoApp.state.todos[0].completed).toBe(true);
+    // additional test for completedAt
+    expect(todoApp.state.todos[0].completedAt).toBeA('number');
+  });
+
+  it('should toggle todo from completed to incompleted', () => {
+    var todoData = {
+      id: 11,
+      text: 'Test features',
+      completed: true,
+      createdAt: 0,
+      completedAt: 123,
+    };
+    var todoApp = TestUtils.renderIntoDocument(<TodoApp />);
+    todoApp.setState({ todos: [todoData] });
+
+    expect(todoApp.state.todos[0].completed).toBe(true);
+    todoApp.handleToggle(11);
+    expect(todoApp.state.todos[0].completed).toBe(false);
+    expect(todoApp.state.todos[0].completedAt).toNotExist();
   });
 });
